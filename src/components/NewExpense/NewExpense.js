a09a7c6911f6c6ef37207975895672f2b74a170a
rohansethi87 <rohan.sethi87@gmail.com>
2021-09-23 20:42:53 +0530

    Added conditional content and multiple states for form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
@@ -10,11 +10,23 @@ const NewExpense = (props) => {
       id: Math.random().toString()
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const startEditingForm = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingForm = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (<button onClick={startEditingForm}>Add Expense</button>)}
+      {isEditing && (<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingForm} />)}
     </div>
   );
 };
